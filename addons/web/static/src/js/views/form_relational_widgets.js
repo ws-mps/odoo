86414d7075ff17e320dd580a61eed821cb72f58e
Michael Brown <mbrown@fensystems.co.uk>
2016-09-09 01:09:55 +0100

    [IMP] widgets: allow for multi-selection in lists
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- addons/web/static/src/js/views/form_relational_widgets.js
+++ addons/web/static/src/js/views/form_relational_widgets.js
@@ -692,7 +692,6 @@ var AbstractManyField = common.AbstractField.extend({
 });
 
 var FieldX2Many = AbstractManyField.extend({
-    multi_selection: false,
     disable_utility_classes: true,
     x2many_views: {},
     view_options: {},
@@ -746,7 +745,7 @@ var FieldX2Many = AbstractManyField.extend({
                 _.extend(view.options, {
                     action_buttons: false, // to avoid 'Save' and 'Discard' buttons to appear in X2M fields
                     addable: null,
-                    selectable: self.multi_selection,
+                    selectable: self.options.multi_selection,
                     sortable: true,
                     import_enabled: false,
                     deletable: true
